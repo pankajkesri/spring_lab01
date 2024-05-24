7cac866f032dd8dacffb2fff3a90aa7ec6ac14bf
pankajkesri <156541463+pankajkesri@users.noreply.github.com>
2024-05-24 16:36:06 -0400

    added few lines in both the scripts

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Special cases aren't special enough to break the rules.")
+print("Although practicality beats purity.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("In the face of ambiguity, refuse the temptation to guess.")
+print("There should be one-- and preferably only one --obvious way to do it.")
