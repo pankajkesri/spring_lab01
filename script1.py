e0d4740bb3f709f33018fb8090c79af3cb2a95c9
pankajkesri <156541463+pankajkesri@users.noreply.github.com>
2024-05-24 16:45:24 -0400

    Revert "modified both the scripting files"
    This reverts commit 0fabb5e145dee26036373f9720feca22767304a2.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # Print the lines from Zen of Python
 print("Special cases aren't special enough to break the rules.")
+print("Although practicality beats purity.")
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# Print the lines from Zen of Python
 print("In the face of ambiguity, refuse the temptation to guess.")
 print("There should be one-- and preferably only one --obvious way to do it.")
